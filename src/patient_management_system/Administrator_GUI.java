e9dc3106d423784f0e6f61cd0953318cd2c5e841
Josh <Josh@Josh-DESKTOP>
2020-07-23 12:14:05 +0100

    Mains removed from all forms

--- src/patient_management_system/Administrator_GUI.java
+++ src/patient_management_system/Administrator_GUI.java
@@ -155,7 +155,7 @@ public class Administrator_GUI extends javax.swing.JFrame {
     }//GEN-LAST:event_btnCreateAdministratorActionPerformed
 
     private void btnDoctorRatingsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDoctorRatingsActionPerformed
-        AdminViewDoctorRating_GUI open = new AdminViewDoctorRating_GUI();
+        AdminViewDoctorRatingAndProvideFeedBack_GUI open = new AdminViewDoctorRatingAndProvideFeedBack_GUI();
         open.setVisible(true);
         this.setVisible(false);
         this.dispose();
@@ -181,41 +181,7 @@ public class Administrator_GUI extends javax.swing.JFrame {
         
     }//GEN-LAST:event_btnRemoveActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Administrator_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Administrator_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Administrator_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Administrator_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Administrator_GUI().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdd;
